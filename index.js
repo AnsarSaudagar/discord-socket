ef244a62072201b3cf50a01067044243331af6ae
ansar <ansarsaudagar40@gmail.com>
2024-12-09 00:26:10 +0530

    basic code refractoring

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(cors());
 const io = new Server(server, {
   cors: {
-    origin: "*", // Allow all origins (adjust for security in production)
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
@@ -33,8 +33,6 @@ app.get('/new-message/:userId', (req, res) => {
     const userId = req.params.userId;
     const socketId = userSockets[userId];
     if(socketId){
-        console.log("new message");
-        
         io.to(socketId).emit("new_message");
     }
     return res.json({
@@ -42,11 +40,11 @@ app.get('/new-message/:userId', (req, res) => {
     })
 });
 
-app.get("/", (req, res) => {
-  res.json({
-    ansar: true,
-  });
-});
+// app.get("/", (req, res) => {
+//   res.json({
+//     ansar: true,
+//   });
+// });
 
 server.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
